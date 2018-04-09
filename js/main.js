e67566e11e9c69dc347ee4726d37e529863f9296
tunime <tunime.1@gmail.com>
2018-04-09 10:44:11 -0500

    solving errors

--- js/main.js
+++ js/main.js
@@ -40,14 +40,14 @@ var shsopots=[];
             render(datos);
             function render (datos) {
                 var html = datos.map(function(elem, index) {
-                  return('<div id="posts" style="top:'+elem.scaleY+'px; left:'+elem.scaleX+'px;">'+elem.sid+'</div><div style="top:'+elem.scaleY+'px; left:'+elem.scaleX+'px;" class="card"><h2>'+elem.stitle+'</h2><h3>'+elem.ssubtitle+'</h3><p>'+elem.sdescription+'</p><div class="firure"><img class="admin--view--img" src="'+elem.surl+'" alt=""></div><div>');
+                  return('<div id="posts" style="top:'+elem.scaleY+'px; left:'+elem.scaleX+'px;">'+elem.sid+'</div><div style="top:'+elem.scaleY+'px; left:'+elem.scaleX+'px;" class="card"><h2>'+elem.stitle+'</h2><h3>'+elem.ssubtitle+'</h3><p>'+elem.sdescription+'</p><div class="firure"><img class="admin--view--img" src="'+elem.surl+'" alt=""></div></div>');
               }).join(" ");
               $('#witgetimg').append(html);
             }
         },
         methods:{
             newhospot:function(event){
-                $("#btnadd").css("display", "block");
+                $("#btnadd").css("display", "flex");
                 $('#forinputs').append('<div id="alvarus" class="admin--edit--add"><input id="scaley" type="text" class="admin__input" value="'+event.clientY+'"><input id="scalex" type="text" class="admin__input" value="'+event.clientX+'"><input id="mstitle" type="text" class="admin__input" placeholder="Title"><input id="mssubtitle" type="text" class="admin__input" placeholder="Subtitle"><textarea id="msdescription" class="admin__input" id="exampleFormControlTextarea1" rows="3" placeholder="Descripcion"></textarea><input id="msurl" type="text" class="admin__input" placeholder="img URL"></div>');
                 $('#witgetimg').append('<div id="posts" style="top:'+event.clientY+'px; left:'+event.clientX+'px;"></div>');
                 
